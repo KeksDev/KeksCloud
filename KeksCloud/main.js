88481ba130bd31d80af8c622ab46a718b3c6a3d6
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2021-02-01 10:52:15 +0100

    Create main.js

---
+++ KeksCloud/main.js
@@ -0,0 +1,8 @@
+/**
+ * Lol, this is just a Prank. 
+ * You don't really think that I give YOU my KeksCloud?
+ * Nice try btw.
+ * 
+ * KeksDevelopment: https://discord.gg/zW6jpzVRZc
+ * Discord: KeksDev#9513
+ */
